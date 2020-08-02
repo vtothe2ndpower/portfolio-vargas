81993cc88028f3bf4092cc799135410c9fa1789b
EC2 Default User <ec2-user@ip-172-31-24-31.us-east-2.compute.internal>
2020-08-02 01:39:25 +0000

    Add structure to files within application

---
+++ app.js
@@ -0,0 +1,28 @@
+var express = require('express'),
+app         = express();
+
+app.set("view engine", "ejs");
+
+app.use(express.static(__dirname + '/public/'));
+
+// Routes
+app.get("/", function(req, res){
+    res.render("home");
+});
+
+app.get("/about", function(req, res){
+    res.render("about");
+});
+
+app.get("/projects", function(req, res){
+    res.render("projects");
+});
+
+app.get("/services", function(req, res){
+    res.render("services");
+});
+
+
+app.listen(process.env.PORT, process.env.IP, function(){
+   console.log("Portfolio Server Has Started!"); 
+});
